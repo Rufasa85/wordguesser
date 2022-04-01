7a7fd8b8df2e9333a547a67fb342ad76de3da1c1
Joe Rehfuss <joe.rehfuss@gmail.com>
2022-04-01 15:14:03 -0700

    adding timer

--- script.js
+++ script.js
@@ -1,32 +1,74 @@
 //generate random word to guess
-    //create a collection of words
-var wordPara = document.querySelector("#wordPara")
-var words = ["manatee","zebra","lumpsucker","syzygy","kiwi"]
-    //show user "_" for each letter in word
-var chosenWord = words[Math.floor(Math.random()*words.length)]
-chosenWord = chosenWord.split("")
-console.log("chosenword:", chosenWord)
+//create a collection of words
+var wordPara = document.querySelector("#wordPara");
+var startBtn = document.querySelector("#start");
+var timePara = document.querySelector("#timePara")
+var words = ["manatee", "zebra", "lumpsucker", "syzygy", "kiwi"];
+var chosenWord;
+var isPlaying = false;
 var userGuessArray = [];
-for (let i = 0; i < chosenWord.length; i++) {
-    userGuessArray.push("_")
+var timer;
+var timeLeft=10;
+//show user "_" for each letter in word
+function startGame() {
+    isPlaying=true
+    userGuessArray=[];
+  chosenWord = words[Math.floor(Math.random() * words.length)];
+  chosenWord = chosenWord.split("");
+  console.log("chosenword:", chosenWord);
+  for (let i = 0; i < chosenWord.length; i++) {
+    userGuessArray.push("_");
+  }
+  console.log("userGuess:", userGuessArray);
+  wordPara.textContent = userGuessArray.join(" ");
+  startTimer()
 }
-console.log("userGuess:",userGuessArray)
-wordPara.textContent = userGuessArray.join(" ")
-//listen for keypress
-document.addEventListener("keyup",function(event){
-    var guessedLetter = event.key.toLowerCase()
-    console.log("guessed:",guessedLetter)
-    if(chosenWord.includes(guessedLetter)){
-        // if pressed key is in word, update shown word
-        for (let i = 0; i < chosenWord.length; i++) {
-            if(chosenWord[i]===guessedLetter){
-                userGuessArray[i]=guessedLetter
-            }    
+
+function startTimer() {
+    timer = setInterval(function(){
+        console.log(timeLeft);
+        timeLeft--;
+        timePara.textContent=timeLeft
+        if(timeLeft<=0){
+            clearInterval(timer);
+            console.log("LOST!")
         }
-        console.log("LETTER IS IN WORD!!!!!")
-        console.log("updated userGuess",userGuessArray)
-        wordPara.textContent = userGuessArray.join(" ")
-    }
-})
+    },1000)
+}
+
 //if won, add win
-    //when users guess matches word, they won
+//when users guess matches word, they won
+function checkWin() {
+  if (userGuessArray.join("") === chosenWord.join("")) {
+      isPlaying = false;
+      clearInterval(timer);
+    return true;
+  } else {
+    return false;
+  }
+}
+
+startBtn.addEventListener("click",startGame);
+
+//listen for keypress
+document.addEventListener("keyup", function(event) {
+    if (!isPlaying) {
+      return;
+    }
+    var guessedLetter = event.key.toLowerCase();
+    console.log("guessed:", guessedLetter);
+    if (chosenWord.includes(guessedLetter)) {
+      // if pressed key is in word, update shown word
+      for (let i = 0; i < chosenWord.length; i++) {
+        if (chosenWord[i] === guessedLetter) {
+          userGuessArray[i] = guessedLetter;
+        }
+      }
+      console.log("LETTER IS IN WORD!!!!!");
+      console.log("updated userGuess", userGuessArray);
+      wordPara.textContent = userGuessArray.join(" ");
+      if (checkWin()) {
+        console.log("WINNER!");
+      }
+    }
+  });
